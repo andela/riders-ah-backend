4dbcd498d85f1bf7a43d4bd554753ff760a3ba19
Niyitangasam <niyitangasam@gmail.com>
2019-05-06 12:13:04 +0200

    fix(#165413104): Fix eslint jsdoc commment
    Delivers(#165413104)

--- config/passport/index.js
+++ config/passport/index.js
@@ -1,9 +1,15 @@
-/* eslint-disable require-jsdoc */
 import passport from 'passport';
 import { facebookStrategy, googleStrategy, twitterStrategy } from './strategy';
 import { facebookTestStrategy, twitterTestStrategy, googleTestStrategy } from './testStrategy';
-
+/**
+ * @exports Strategy
+ * @class Strategy
+ * @description Handles all strategies
+ * */
 class Strategy {
+  /**
+     * Initialiaze all passport
+     */
   constructor() {
     this.facebookStrategy = passport.use(facebookStrategy);
     this.googleStrategy = passport.use(googleStrategy);
@@ -14,6 +20,12 @@ class Strategy {
     this.strategy = null;
   }
 
+  /**
+     * Check the environment
+     * @function strategyTouse
+     * @param  {string} strategy - accept social network to use
+     * @return {string} Returns strategy
+     */
   strategyTouse(strategy) {
     if (process.env.NODE_ENV === 'test') {
       this.strategy = `${strategy}-test`;
--- helpers/index.js
+++ helpers/index.js
@@ -1,21 +1,44 @@
-/* eslint-disable require-jsdoc */
 import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
 dotenv.config();
+/**
+ * @exports Helper
+ * @class Helper
+ * @description Helps to generate token and passwor hashing
+ * */
 class Helper {
+  /**
+     * Check the environment
+     * @function generateToken
+     * @param  {object} userinfo - accept object with user info
+     * @return {string} Returns strategy
+     */
   static generateToken(userinfo) {
     const Issuetoken = jwt.sign(userinfo,
       process.env.SECRET, { expiresIn: '1d' });
     return Issuetoken;
   }
 
+  /**
+     * Check the environment
+     * @function hashPassword
+     * @param  {string} password - user password
+     * @return {string} Returns hashedPassword
+     */
   static hashPassword(password) {
     const hashedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(8));
     return hashedPassword;
   }
 
+  /**
+     * Check the environment
+     * @function comparePassword
+     * @param  {string} passwordHash - a hashed  password
+     * @param  {string} password - a user  password
+     * @return {boolean} comparedPassword -Returns true if they are matching or false
+     */
   static comparePassword(passwordHash, password) {
     const comparedPassword = bcrypt.compareSync(password, passwordHash);
     return comparedPassword;
