9a01cf860b35388998926248f084459a8df2d476
Jean dAmour AKIMANIZANYE <jean.damour-akimanizanye@andela.com>
2019-06-20 14:14:29 +0200

    Add socket

--- helpers/utils/socketIo.js
+++ helpers/utils/socketIo.js
@@ -12,28 +12,35 @@ const { Message, User } = models;
  * @description Implemetation on socket
  */
 const SocketIO = (app) => {
-  http.createServer(app);
-  const port = process.env.PORT || 3000;
-  const io = socketIo.listen(app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-  }));
-  io.use((socket, next) => {
-    next(null, next);
-  });
-  io.on('connection', (socket) => {
-    socket.on('new_message', async (data) => {
-      const userId = await userHelper.findUserByToken(data.token);
-      const user = await User.findOne({ where: { id: userId } });
-      const { username } = user.dataValues;
-      const saveMessage = await Message.create({
-        senderId: userId,
-        message: data.message
-      });
-      if (!saveMessage) socket.emit('message_failed', { error: 'Last message was not sent' });
-      socket.emit('message_created', { message: data.message, username });
-    });
-    socket.on('disconnect', () => {});
-  });
+	http.createServer(app);
+	const port = process.env.PORT || 3000;
+	const io = socketIo.listen(
+		app.listen(port, () => {
+			console.log(`Listening on port ${port}`);
+		})
+	);
+	io.use((socket, next) => {
+		next(null, next);
+	});
+	io.on('connection', (socket) => {
+		socket.on('create', (name) => socket.join(name));
+		socket.on('auto_submit', (roomName) => {
+			io.sockets.in(roomName).emit('submit', { isSubmited: true, user: 'test' });
+			console.log('====>Auto Submit called');
+		});
+		socket.on('new_message', async (data) => {
+			const userId = await userHelper.findUserByToken(data.token);
+			const user = await User.findOne({ where: { id: userId } });
+			const { username } = user.dataValues;
+			const saveMessage = await Message.create({
+				senderId: userId,
+				message: data.message
+			});
+			if (!saveMessage) socket.emit('message_failed', { error: 'Last message was not sent' });
+			socket.emit('message_created', { message: data.message, username });
+		});
+		socket.on('disconnect', () => {});
+	});
 };
 
 export default SocketIO;
