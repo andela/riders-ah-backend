e800c607b7b59fb9567a189d89da6c7cf21be489
Kabalisa <43787407+Kabalisa@users.noreply.github.com>
2019-08-05 11:10:22 +0200

    chore(bookmark): change on bookmark feature (#69)
    - when fetching bookmarks, I need more information about the articles like image, reading time and the time it was created
    - I also need the username of an article
    
    [delivers #167553045]

--- routes/api/article/doc.js
+++ routes/api/article/doc.js
@@ -129,7 +129,7 @@
  *         description: Has not permission to create tag
  *       '404':
  *         description: Article not found
-  * @swagger
+ * @swagger
  * /articles/tag/list:
  *   get:
  *     tags:
@@ -668,74 +668,74 @@
  *         type: text
  */
 /**
-* @swagger
-*  /articles/{slug}/report/{reportType}:
-*   post:
-*     tags:
-*       - Report
-*     name: Report
-*     summary: User must be able to report an article.
-*     produces:
-*       - application/json
-*     consumes:
-*       - application/json
-*     parameters:
-*       - name: slug
-*         in: path
-*         schema:
-*           type: string
-*         required:
-*           - slug
-*       - name: reportType
-*         in: path
-*         schema:
-*           type: string
-*         required:
-*           - reportType
-*       - name: authorization
-*         in: header
-*         schema:
-*           type: string
-*         required:
-*           - authorization
-*       - name: reason
-*         in: reason
-*         schema:
-*           $ref: '#/definitions/ReportedArticle'
-*           type: object
-*           properties:
-*             reason:
-*               type: text
-*     responses:
-*       '201':
-*         description: Article reported
-*       '400':
-*         description: Bad Request
-*       '404':
-*         description: article not found
-*/
+ * @swagger
+ *  /articles/{slug}/report/{reportType}:
+ *   post:
+ *     tags:
+ *       - Report
+ *     name: Report
+ *     summary: User must be able to report an article.
+ *     produces:
+ *       - application/json
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - name: slug
+ *         in: path
+ *         schema:
+ *           type: string
+ *         required:
+ *           - slug
+ *       - name: reportType
+ *         in: path
+ *         schema:
+ *           type: string
+ *         required:
+ *           - reportType
+ *       - name: authorization
+ *         in: header
+ *         schema:
+ *           type: string
+ *         required:
+ *           - authorization
+ *       - name: reason
+ *         in: reason
+ *         schema:
+ *           $ref: '#/definitions/ReportedArticle'
+ *           type: object
+ *           properties:
+ *             reason:
+ *               type: text
+ *     responses:
+ *       '201':
+ *         description: Article reported
+ *       '400':
+ *         description: Bad Request
+ *       '404':
+ *         description: article not found
+ */
 /**
-* @swagger
-*  /articles/reported:
-*   get:
-*     tags:
-*       - Report
-*     name: Report
-*     summary: Get all reported articles
-*     produces:
-*       - application/json
-*     consumes:
-*       - application/json
-*     parameters:
-*       - name: authorization
-*         in: header
-*         schema:
-*           type: string
-*         required:
-*           - authorization
-*     responses:
-*       '200':
-*         description: ok
-*       '404':
-*         description: article not found
-*/
+ * @swagger
+ *  /articles/reported:
+ *   get:
+ *     tags:
+ *       - Report
+ *     name: Report
+ *     summary: Get all reported articles
+ *     produces:
+ *       - application/json
+ *     consumes:
+ *       - application/json
+ *     parameters:
+ *       - name: authorization
+ *         in: header
+ *         schema:
+ *           type: string
+ *         required:
+ *           - authorization
+ *     responses:
+ *       '200':
+ *         description: ok
+ *       '404':
+ *         description: article not found
+ */
